59ea273ff560b1046b2904d86cc608adf1043a2c
Patzsopd <95394814+Patzsopd@users.noreply.github.com>
2023-06-30 15:44:12 -0400

    Update Configuration.py

--- app_files/radar_control/Class/Configuration.py
+++ app_files/radar_control/Class/Configuration.py
@@ -72,10 +72,10 @@ class SigProConfig:
         self.detection_thresh = -15 # dB TODO: finalize value or make configurable
         self.zone_number      = 3
 
-        if dds_enabled is True:
+        #if dds_enabled is True:
             
-        else:
-            self.dds_enabled = False
+        #else:
+            #self.dds_enabled = False
 
         self.dds_enabled = True
         self.radar_report_writer = RadarReport.RadarReportWriter()
