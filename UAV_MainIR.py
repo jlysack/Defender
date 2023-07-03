e961dc9d77cda18b0e781671599d31ae4699f4e5
Patzsopd <95394814+Patzsopd@users.noreply.github.com>
2023-07-03 13:51:38 -0400

    Create UAV_MainIR.py

---
+++ UAV_MainIR.py
@@ -0,0 +1,49 @@
+#import pylirc
+import time
+import sys
+import rti.connextdds as dds
+import rti.asyncio
+import asyncio
+import aiofiles
+from interfaces import DDS
+import subprocess
+
+#Participant
+participant = dds.DomainParticipant(domain_id=1)
+
+#Topics
+HitDetection_topic = dds.Topic(participant, "HitDetection", DDS.Weapon.HitDetection)
+
+#Writers
+HitDetection_writer = dds.DataWriter(participant.implicit_publisher, HitDetection_topic)
+
+#Creating Global Data Holders
+HitDetection_data = DDS.Weapon.HitDetection
+HitDetection_data.HitBoolean = False
+HitDetection_data.HitNumber = 0
+
+#sockid = pylirc.init("IRReceiver.py", blocking=False)
+process = subprocess.Popen(["irw"], stdout=subprocess.PIPE)
+
+try:
+    while True:
+        # Read incoming IR signals
+        #signals = pylirc.nextcode()
+        #output = subprocess.check_output(["irw"]).decode("utf-8")
+        output = process.stdout.readline().decode("utf-8")
+
+        #if signals:
+            #for signal in singals:
+                #print("Received Signal:", signal)
+
+        if output.strip():
+            print("Received signal:", output.strip())
+
+
+except KeyboardInterrupt:
+    pass
+
+
+# Clean up the LIRC Connection
+#pylirc.deinit()
+process.terminate()
