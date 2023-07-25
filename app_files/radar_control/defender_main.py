d532759a13d2288ccd644ed9f87013d6455361c4
Reid Moak <reid.moak@gmail.com>
2023-07-25 02:03:27 +0000

    Initial radar health message functionality

--- app_files/radar_control/defender_main.py
+++ app_files/radar_control/defender_main.py
@@ -16,6 +16,7 @@ from dds_listeners import ScanInstructionListener
 import multiprocessing
 from multiprocessing import Process
 import time
+import threading
  
 def print_zone_info(zone):
     for field in const.ZONES[zone]:
@@ -30,7 +31,7 @@ def m_to_feet(meters):
 async def get_scan_instruction(dds_listener):
     return await dds_listener.get_data()
 
-async def main_execution_loop():
+async def main_execution_loop(health_message_writer):
     dds_enabled = True
     debug_enabled = False
 
@@ -106,15 +107,56 @@ async def main_execution_loop():
             # Create and start process for radar_search, then return to the top to listen for the next scan instruction
             radar_process = Process(target=radar_control.radar_search, args=(Brd, sigpro_cfg, plot_cfg, process_queue))
             radar_process.start()
+
+            # Send health message
+            
+            send_health_message(health_message_writer, time.time(), 1)
+
     except KeyboardInterrupt:
         print("Ctrl+C Detected in defender_main.py main_execution_loop()")
         sys.exit(0)
 
+def send_health_message(writer, time, state=1):
+    global saved_time
+
+    if time - saved_time >= 10.0:
+        componentHealth_data = DDS.Metrics.ComponentHealth
+        componentHealth_data.Name = "TA_R"
+        componentHealth_data.State = state
+
+        writer.write(componentHealth_data)
+        saved_time = time
+
+    print("Inside send_health_message")
+    return True
+    
+
 if __name__ == "__main__":
-    safety_process = sp.Popen('./safety_message_handler.py')
+    global saved_time
+    saved_time = time.time()
+
+    participant = dds.DomainParticipant(domain_id=1)
+
+    # Topic
+    health_message_topic = dds.Topic(participant, "ComponentHealth", DDS.Metrics.ComponentHealth)
+
+    # Writer
+    health_message_writer = dds.DataWriter(participant.implicit_publisher, health_message_topic)
 
     try:
-        asyncio.run(main_execution_loop())
+        safety_process = sp.Popen('./safety_message_handler.py')
+        asyncio.run(main_execution_loop(health_message_writer))
     except KeyboardInterrupt:
         print("Ctrl+C Detected in defender_main.py __main__(). Exiting.")
+        # Kill health_message writer
+        #health_message_timer.cancel()
+        # write a 0
+        send_health_message(health_message_writer, 0)
+        sys.exit(0)
+    except Exception as e:
+        print(e)
+        # Kill health_message writer
+        #health_message_timer.cancel()
+        # write a 0
+        send_health_message(health_message_writer, 0)
         sys.exit(0)
