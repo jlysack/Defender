a81b506c1d2e7408e2ca72f661af7c4c67c8b661
Patzsopd <95394814+Patzsopd@users.noreply.github.com>
2023-08-02 21:26:03 -0400

    Update UAV_MainIR.py

--- UAV_MainIR.py
+++ UAV_MainIR.py
@@ -28,29 +28,70 @@ componentHealth_data = DDS.Metrics.ComponentHealth
 componentHealth_data.Name = "UAV_IR"
 componentHealth_data.State = 1
 
-process = subprocess.Popen(["irw"], stdout=subprocess.PIPE)
+##process = subprocess.Popen(["irw"], stdout=subprocess.PIPE)
 
-async def main_execution_loop():
+##async def main_execution_loop():
+##    try:
+##        while True:
+##            output = process.stdout.readline().decode("utf-8")
+##
+##            if output.strip():
+##                print("Received signal:", output.strip())
+##
+##                HitDetection_data.HitBoolean = True
+##
+##                HitDetection_writer.write(HitDetection_data)
+##
+##            HitDetection_data.HitBoolean = False
+##
+##    except KeyboardInterrupt:
+##        componentHealth_data.State = 0
+##        componentHealth_writer.write(componentHealth_data)
+##
+##        # Clean up the LIRC Connection
+##        process.terminate()
+##        
+##        #pass
+
+async def read_irw_output():
+    process = await asyncio.create_subprocess_exec(
+        "irw",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
     try:
         while True:
-            output = process.stdout.readline().decode("utf-8")
+            output = await process.stdout.readlines()
+            if not output:
+                break
 
-            if output.strip():
-                print("Received signal:", output.strip())
+            line = output.decode("utf-8").strip()
+            if line:
+                print("Recieved IR signal:", line)
 
                 HitDetection_data.HitBoolean = True
-
                 HitDetection_writer.write(HitDetection_data)
 
             HitDetection_data.HitBoolean = False
 
+    except asyncio.CancelledError:
+        process.terminate()
+        await process.wait()
     except Exception as e:
-            print(f"Error in writing componentHealth_data: {e}")
+        print(f"Error while reading irw output: {e}")
+    finally:
+        process.terminate()
+        await process.wait()
 
-##    except KeyboardInterrupt:
-##        componentHealth_data.State = 0
-##        componentHealth_writer.write(componentHealth_data)
-##        #pass
+async def main_execution_loop():
+    try:
+        await read_irw_output()
+    except asyncio.CancelledError:
+        pass
+    finally:
+        componentHealth_data.State = 0
+        componentHealth_writer.write(componentHealth_data)
 
 async def main_loop():
     global componentHealth_data
@@ -90,6 +131,3 @@ async def run_event_loop():
     await asyncio.gather(*tasks)
 
 asyncio.run(run_event_loop())
-
-# Clean up the LIRC Connection
-process.terminate()
